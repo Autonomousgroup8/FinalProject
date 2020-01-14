144c4f87cf683b7cd286861af42bdb34fab1434c
Mitchell van Es <mitchellvanes@gmail.com>
2020-01-14 14:16:59 +0100

    Update Driving.py

--- Python files/python_sample/Driving.py
+++ Python files/python_sample/Driving.py
@@ -5,7 +5,7 @@ from lib.memegenome import MemeGenome
 from lib.memesimcommand import MemeSimCommand
 from lib.memesimresponse import MemeSimResponse
 from lib.memesimclient import MemeSimClient
-#zig from zigbee import Zigbee
+from zigbee import Zigbee
 import Locations
 sign = lambda x: math.copysign(1, x)
 
@@ -14,11 +14,6 @@ distance_tolerance = 50
 angles = [2, 5, 10, 15, 20, 30, 45, 90, 135, 180]
 distances = [20, 50, 100, 150, 200, 300, 400, 500, 750, 1000]
 
-
-#MOVE SIM PARAM
-delta_t = 0.001
-speed = 10*delta_t
-
 # Server connection setup
 MEMESIM_IP_ADDR = "131.155.127.244"
 TEAM_NUMBER = 8
@@ -26,43 +21,7 @@ MEMESIM_CLIENT = MemeSimClient(MEMESIM_IP_ADDR, TEAM_NUMBER)
 MEMESIM_CLIENT.connect()
 
 # Connect Zigbee
-#zig ZIGBEE = Zigbee('COM20', 9600)
-
-def SetPosition(robotID, position):
-    RQ = MemeSimCommand.RS(8, robotID+14 ,position[0] ,position[1] ,math.radians(position[2]))
-    MEMESIM_CLIENT.send_command(RQ)
-
-    sleep(1.0)
-
-    RQ = MemeSimCommand.RQ(8, robotID+14)
-    MEMESIM_CLIENT.send_command(RQ)
-
-    sleep(1.0)
-
-    RESPONSES = MEMESIM_CLIENT.new_responses()
-
-    for resp in RESPONSES:
-        if resp.cmdtype() == 'rs':
-            if not resp.iserror():
-                xpos = int(float(resp.cmdargs()[2]))
-                ypos = int(float(resp.cmdargs()[3]))
-                angle = round(math.degrees(float(resp.cmdargs()[4])))
-
-                print(f"Set position of Robot {robotID}: {xpos}, {ypos}, {angle}")
-    return
-
-def simDriving(instruction):
-    rob_pos, rob_angle = GetPosition(RobotID)
-
-    if instruction[0] == "L":
-        newAngle = rob_angle + angles[int(instruction[1])]
-        SetPosition(2, [rob_pos[0], rob_pos[1], newAngle])
-    elif instruction[0] == "R":
-        newAngle = rob_angle - angles[int(instruction[1])]
-        SetPosition(2, [rob_pos[0], rob_pos[1], newAngle])
-    elif instruction[0] == "F":
-        newPos = [rob_pos[0] + distances[int(instruction[1])]*math.cos(math.radians(rob_angle)), rob_pos[1] + distances[int(instruction[1])]*math.sin(math.radians(rob_angle))]
-        SetPosition(2, [newPos[0], newPos[1], rob_angle])
+ZIGBEE = Zigbee('COM20', 9600)
 
 def GetPosition(robotID):
     RQ = MemeSimCommand.RQ(8, robotID+14)
@@ -79,13 +38,16 @@ def GetPosition(robotID):
                 ypos = int(float(resp.cmdargs()[3]))
                 angle = round(math.degrees(float(resp.cmdargs()[4])))
 
+                if abs(angle) > 180:
+                    angle = angle - sign(angle) * 360
+
                 return [xpos, ypos], angle
     return
 
 def GetInstruction(RobotID, target):
     try:
         rob_pos, rob_angle = GetPosition(RobotID)
-        print([round(rob_pos[0], 1), round(rob_pos[1], 1)], round(rob_angle, 1))
+        print(f"Robot position: [{round(rob_pos[0], 1)}, {round(rob_pos[1], 1)}], {round(rob_angle, 1)}")
     except:
         return "ERROR: No Position"
 
@@ -95,8 +57,7 @@ def GetInstruction(RobotID, target):
 
     tar_distance = math.sqrt((target[1] - rob_pos[1])**2 + (target[0] - rob_pos[0])**2)
 
-    print(f"Target angle = {tar_angle}")
-    print(f"Target distance = {tar_distance}")
+    print(f"Target angle = {tar_angle}, Target distance = {tar_distance}")
 
     if tar_distance > distance_tolerance:
         if abs(tar_angle) > angle_tolerance:
@@ -119,7 +80,7 @@ def GetInstruction(RobotID, target):
 def SendInstruction(RobotID, instruction):
     ReveiverID = [5, 5][RobotID-1]
     print("Send Instruction: " + "1" + str(ReveiverID) + instruction)
-    #zig ZIGBEE.write(bytes("1" + str(ReveiverID) + instruction, 'utf-8'))
+    ZIGBEE.write(bytes("1" + str(ReveiverID) + instruction, 'utf-8'))
     simDriving(instruction)
 
 def GuideTo(RobotID, target):
@@ -141,8 +102,8 @@ def GuideTo(RobotID, target):
 
 # Init position
 RobotID = 2
-target = [0,500]
+target = [0,0]
 
 #print(GetPosition(RobotID))
-SetPosition(RobotID, [500, 0, -90])
+
 GuideTo(RobotID, target)
