75eae7068d6127918f68401a1a2795b2be4d1815
Lninol <47011024+Lninol@users.noreply.github.com>
2020-01-15 16:44:10 +0100

    Added dictionary support
    hell yea

--- Python files/python_sample/Locations.py
+++ Python files/python_sample/Locations.py
@@ -24,3 +24,30 @@ MiddleLabland = [750, 750]
 MiddleEurope = [2750, 750]
 MiddleAfrica = [2750, 2750]
 MiddleAmerica = [750, 2750]
+
+locDict = {
+"Lab8" : [175, 1325],
+"FunnelLabEuropa1" : [1450, 750],
+"FunnelLabEuropa2" : [2150, 750],
+"FunnelEuropaAfrica1" : [2750, 1250],
+"FunnelEuropaAfrica2" : [2750, 1950],
+"FunnelAfricaAmerica1" : [2150, 2750],
+"FunnelAfricaAmerica2" : [1450, 2750],
+"FunnelAmericaLab1" : [750, 1950],
+"FunnelAmericaLab2" : [750, 1250],
+"C01" : [2550, 250],
+"C02" : [3250, 250],
+"C03" : [3250, 950],
+"C04" : [2250, 1250],
+"C05" : [3250, 2550],
+"C06" : [3250, 3250],
+"C07" : [2550, 3250],
+"C08" : [2250, 2250],
+"C09" : [950, 3250],
+"C10" : [250, 3250],
+"C11" : [250, 2550],
+"C12" : [1250, 2250],
+"MiddleLabland" : [750, 750],
+"MiddleEurope" : [2750, 750],
+"MiddleAfrica" : [2750, 2750],
+"MiddleAmerica" : [750, 2750]}
